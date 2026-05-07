06783fca0d15a5cb815b61ed986976e1d10d40ec
anasyousaf-dev <anasyousaf440@email.com>
2026-05-08 00:14:50 +0500

    feat: add chalk library with TypeScript definitions and utilities
    - Introduced chalk library for styled terminal output.
    - Added TypeScript definitions for chalk in index.d.ts.
    - Implemented main chalk functionality in index.js.
    - Created utility functions for string manipulation in utilities.js.
    - Defined ANSI styles and color support in vendor/ansi-styles and vendor/supports-color.
    - Added browser compatibility for color support detection.
    - Established package.json and package-lock.json for project dependencies.
    - Created script.js for npm command explanations and usage.

---
+++ Backend-Development-Part3/script.js
@@ -0,0 +1,44 @@
+                                                
+                                                // NPM understanding
+
+// npm -> node package module(which is like a playstore or a store for downloading node modules,it was mainly form to download the node
+// modules but it have so many things module like express modules,java modules etc)
+
+                                               // installing and uninstalling anything basics & advanced
+
+// how to install something from npm?
+// install -> npm i packageKaNaam
+// uninstall -> npm uninstall packageKaNaam  or npm un packageKaNaam
+// installing particular version -> npm i packageKaNaam@version
+
+
+                                                // understanding node_modules
+
+// hamara koi lana dana nhi ha
+// jab ham koi package install karty ha tu wo shaid kaisi or package par package par 
+// dependent ho tu oss case mein node_modules ka folder bhi install hojata ha
+// For Example: when installing express,the node modules also install,becuase express depends on the packages which are in node_modules
+
+
+
+                                                // dependencies
+// packages and packages ki dependencies     
+
+                                               // devDependencies
+
+// aise packages jo sirf development mein kaam aye gye lakin jab app ban jaye ga or 
+// upload hojaye ga tab hum in packages ko use nhi kar rahe hogaye
+
+
+                                              // scripts - understanding default scripts PATH and custom scripts
+ 
+// kai baar ap koi script chalate ho and us script mein ap likhte ho ye command
+// npm start for starting the server
+// npm test for testing the server
+// npm run start for starting the server
+// npm run dev for development
+// npm run concurrent
+
+// Answer: start and test ye jo 2 scripts ha,jab ham node and npm install karty ha tu hamary
+//  Os ky  table(Os hamesha table banata ha for commands)  mein ye 2 scripts mention hojate ha,
+                                            
